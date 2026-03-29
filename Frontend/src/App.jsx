66b3d502002176bb51662fa92f80e1cc856d25f1
Shaikh_Naufil <shaikhnaufil2908@gmail.com>
2026-03-29 15:59:38 +0530

    fixed a type in integration of vercel web analytics

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import HomePage from './components/HomePage'
@@ -7,8 +7,8 @@ import ProjectPage from './components/ProjectPage'
 function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <Routes>
-        <Analytics />
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="projects" element={<ProjectPage />} />
